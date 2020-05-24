7a94a795c17e01e45e6f9b2e98a50c349532804d
yeonu97 <yeonu9464@gmail.com>
2020-05-24 16:09:27 +0900

    유니티 튜토리얼 2. 트랜스폼

---
+++ Unity_Project/Assets/scripts/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject go_camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(go_camera.transform.position, Vector3.up, 100 * Time.deltaTime);
+    }
+}
