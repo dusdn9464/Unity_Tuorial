0fbd61f5e514d9878670e5c62565bca13c90cbaa
yeonu97 <yeonu9464@gmail.com>
2020-05-24 20:09:12 +0900

    4. 컬라이더

---
+++ Unity_Project/Assets/scripts/Test3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test3 : MonoBehaviour
+{
+    private BoxCollider col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position += new Vector3(0, 0, 0.1f);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.position = new Vector3(0, 2, 0);
+    }
+}
