bfb1c6969e0ffb73af3a5cab3cf0502c8e27f824
yeonu97 <yeonu9464@gmail.com>
2020-05-25 02:23:45 +0900

    23. 드래그&드롭

---
+++ Unity_Project/Assets/scripts/Test18.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Test18 : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("Begin Drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        transform.position = eventData.position;
+        Debug.Log("Drag");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("Drop");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("End Drag");
+    }
+}
