f9133d6ef098967312d851cb241a3d35a2a80102
yeonu97 <yeonu9464@gmail.com>
2020-05-24 22:16:07 +0900

    7. 라이트

---
+++ Unity_Project/Assets/scripts/Test6.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test6 : MonoBehaviour
+{
+    private Light theLight;
+
+    private float targetIntesity;
+    private float currentIntesity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theLight = GetComponent<Light>();
+        currentIntesity = theLight.intensity;
+        targetIntesity = Random.Range(0.4f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(targetIntesity - currentIntesity) >= 0.01)
+        {
+            if (targetIntesity - currentIntesity >= 0)
+                currentIntesity += Time.deltaTime * 3f;
+            else
+                currentIntesity -= Time.deltaTime * 3f;
+
+            theLight.intensity = currentIntesity;
+            theLight.range = currentIntesity + 10;
+        }
+        else
+        {
+            targetIntesity = Random.Range(0.4f, 1f);
+        }
+    }
+}
