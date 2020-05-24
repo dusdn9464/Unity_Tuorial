170c30b5fb7f4f94e4dc865de820d4ed6583981a
yeonu97 <yeonu9464@gmail.com>
2020-05-25 00:42:33 +0900

    11. UI의 기본

---
+++ Unity_Project/Assets/scripts/Test10.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test10 : MonoBehaviour
+{
+
+    [SerializeField] private Text txt_name;
+    [SerializeField] private Image img_name;
+    [SerializeField] private Sprite sprite;
+
+    private bool isCoolTime = false;
+
+    private float curretTime = 5f;
+    private float delayTime = 5f;
+
+    void Update()
+    {
+
+        if(isCoolTime)
+        {
+            curretTime -= Time.deltaTime;
+            img_name.fillAmount = curretTime /  delayTime;
+
+            if(curretTime <= 0)
+            {
+                isCoolTime = false;
+                curretTime = 1f;
+                img_name.fillAmount = curretTime;
+            }
+        }
+    }
+    public void Change()
+    {
+        txt_name.text = "변경됨";
+        isCoolTime = true;
+    }
+}
