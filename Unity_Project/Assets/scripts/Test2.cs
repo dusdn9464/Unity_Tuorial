bb836364c10772922d8edafdfb2471f001f1dc7c
yeonu97 <yeonu9464@gmail.com>
2020-05-24 16:44:42 +0900

    3. 리지드 바디

---
+++ Unity_Project/Assets/scripts/Test2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour
+{
+    private Rigidbody myRigid;
+    //private Vector3 rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+        //rotation = this.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //rotation += new Vector3(90, 0, 0) * Time.deltaTime;
+            myRigid.AddExplosionForce(10, this.transform.right, 10);
+        }
+    }
+}
