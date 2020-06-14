402e97e67aed3b9066dea0934126833fe4ad97cb
yeonu97 <yeonu9464@gmail.com>
2020-06-14 17:17:39 +0900

    Skill 3 : 폭발 파편

---
+++ Unity_Skill/Unity_Skill/Assets/Skill3/MouseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray t_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit t_hit;
+            if(Physics.Raycast(t_ray, out t_hit, 100f))
+            {
+                t_hit.transform.GetComponent<Cube>().Explosion();
+            }
+        }
+    }
+
+}
